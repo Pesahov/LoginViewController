cfdf9650d19b46da1c0966caa3b46f34b54ab242
Shilol Pesahov <pesahov777@gmail.com>
2021-12-15 21:19:49 +0200

    Login view version 3

--- LoginViewController/ViewController.swift
+++ LoginViewController/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+let validUserName = "shilol"
+let validPassword = "user"
 
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let text = username.text!
+        let pass = password.text!
+        if segue.identifier == "goToWelcome" && text == validUserName && pass == validPassword {
+            let newVC = segue.destination as! WelcomeViewController
+        
+        } else {
+            let allertController = UIAlertController(title: "Access Denied", message: "Wrong user name or password", preferredStyle: .alert)
+                let action = UIAlertAction(title: "Cancel", style: .cancel) { action in
+                }
+                allertController.addAction(action)
+                self.present(allertController, animated: true, completion: nil)
+            
+        }
+    }
+    
+    
+    @IBAction func forgotUserName(_ sender: Any) {
+    }
+    
+    
+    @IBAction func forgotPassword(_ sender: Any) {
+    }
+    
 }
+
+
+
 
