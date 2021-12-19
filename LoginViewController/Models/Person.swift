46e23d676e0f631f860ba60886e6236d0c69fe6b
Shilol Pesahov <pesahov777@gmail.com>
2021-12-19 17:41:09 +0200

    LoginViewController version 8

---
+++ LoginViewController/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  LoginViewController
+//
+//  Created by Shilol Pesahov on 19/12/2021.
+//
+
+import Foundation
---
+++ LoginViewController/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  LoginViewController
+//
+//  Created by Shilol Pesahov on 19/12/2021.
+//
+
+import Foundation
